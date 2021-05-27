dc70e75c1c22dac7936afec0d7c2916e3b038ec7
Tuğrul Hasan Karabulut <tugrulkarabulut64@gmail.com>
2021-05-27 10:17:36 +0300

    programmatic execution

--- word_counter.py
+++ word_counter.py
@@ -1,9 +1,10 @@
 from mrjob.job import MRJob
-from base_job import BaseJob
 from time import time
 import sys
 
-class WordCounter(BaseJob):
+class WordCounter(MRJob):
+    COLUMN_INDEX = 3
+
     def mapper(self, _, line):
         splitted = line.split(',')
         review_summary = splitted[WordCounter.COLUMN_INDEX]
@@ -12,11 +13,3 @@ class WordCounter(BaseJob):
 
     def reducer(self, key, values):
         yield key, sum(values)
-
-if __name__ == '__main__':
-    WordCounter.set_input_path('preprocessed_sample.csv')
-    WordCounter.set_output_path('review_summary_counts.txt')
-    WordCounter.run()
-    WordCounter.set_output_path('review_detail_counts.txt')
-    WordCounter.set_column_index(5)
-    WordCounter.run()
---
+++ word_counter_exec.py
@@ -0,0 +1,22 @@
+from word_counter import WordCounter
+
+if __name__ == '__main__':
+    w = WordCounter(args=['-r', 'hadoop', '-files', 'hdfs:///input/preprocessed_sample.csv'])
+    with w.make_runner() as runner:
+        runner.run()
+        word_count_dict = {}
+        for key, value in w.parse_output(runner.cat_output()):
+            word_count_dict[key] = value
+
+        print(len(word_count_dict))
+
+        WordCounter.COLUMN_INDEX = 5
+        
+        runner.run()
+        word_count_dict = {}
+        for key, value in w.parse_output(runner.cat_output()):
+            word_count_dict[key] = value
+
+        print(len(word_count_dict))
+
+

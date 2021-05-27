90fd1908f76d6bbc279e7b1c81ed777f11f59eed
Tuğrul Hasan Karabulut <tugrulkarabulut64@gmail.com>
2021-05-27 11:34:10 +0300

    arg bugfix

--- word_counter_exec.py
+++ word_counter_exec.py
@@ -1,7 +1,7 @@
 from word_counter import WordCounter
 
 if __name__ == '__main__':
-    w = WordCounter(args=['-r', 'hadoop', '-files', 'hdfs:///input/preprocessed_sample.csv'])
+    w = WordCounter(args=['-r', 'hadoop', '--files', 'hdfs:///input/preprocessed_sample.csv'])
     with w.make_runner() as runner:
         runner.run()
         word_count_dict = {}
@@ -11,7 +11,7 @@ if __name__ == '__main__':
         print(len(word_count_dict))
 
         WordCounter.COLUMN_INDEX = 5
-        
+
         runner.run()
         word_count_dict = {}
         for key, value in w.parse_output(runner.cat_output()):

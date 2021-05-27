0690a9844121e413ed525b7d1f289bd12044c8b3
Tuğrul Hasan Karabulut <tugrulkarabulut64@gmail.com>
2021-05-27 12:19:02 +0300

    bugfix

--- word_counter.py
+++ word_counter.py
@@ -6,10 +6,13 @@ class WordCounter(MRJob):
     COLUMN_INDEX = 3
 
     def mapper(self, _, line):
-        splitted = line.split(',')
-        review_summary = splitted[WordCounter.COLUMN_INDEX]
-        for word in review_summary.strip().split():
-            yield word, 1
+        try:
+            splitted = line.split(',')
+            review_summary = splitted[WordCounter.COLUMN_INDEX]
+            for word in review_summary.strip().split():
+                yield word, 1
+        except:
+            pass
 
     def reducer(self, key, values):
         yield key, sum(values)
--- word_counter_exec.py
+++ word_counter_exec.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 from word_counter import WordCounter
 
 if __name__ == '__main__':

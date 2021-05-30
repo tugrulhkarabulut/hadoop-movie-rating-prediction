ab6d6c4275806c1b6c81b497917be5f2fcf1644a
Tuğrul Hasan Karabulut <tugrulkarabulut64@gmail.com>
2021-05-30 21:17:30 +0300

    attribute splitter job bugfix test

--- attribute_splitter.py
+++ attribute_splitter.py
@@ -18,14 +18,16 @@ class AttributeSplitter(MRJob):
         #from proj.decision_tree import calculate_split_result
         #from proj.random_forest_classifier import RandomForestClassifier
         try:
-            yield (int(line), 0), 0.5
+            yield line, (0, 0.5)
         except:
             pass
 
     def reducer(self, key, values):
-        values = list(values)
-        values.sort(key=lambda x: x[1])
-        yield key, (values[0][0], values[0][1])
+        #values = list(values)
+        #values.sort(key=lambda x: x[1])
+        #yield key, (values[0][0], values[0][1])
+        res = min([v[1] for v in values])
+        yield key, res
 
 
 if __name__ == '__main__':

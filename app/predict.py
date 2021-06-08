2e7e92949d62997af797e4549259a6c06bf955d5
Tuğrul Hasan Karabulut <tugrulkarabulut64@gmail.com>
2021-06-08 12:35:25 +0300

    predict front end implemented. get model endpoint implemented

--- app/predict.py
+++ app/predict.py
@@ -1,11 +1,27 @@
 from flask import Blueprint
 from flask import request
+import os
 
 bp = Blueprint('predict', __name__)
-
 
 @bp.route('/predict', methods=['POST'])
 def build():
     req_data = request.get_json()
     a = {'id': 123}
     return a
+
+
+@bp.route('/get-models', methods=['GET'])
+def get_models():
+    models = []
+    model_names = os.listdir('./app_data')
+
+    for m in model_names:
+        if os.listdir('./app_data/' + m):
+            models.append(m)
+    
+
+    res = {}
+    res['models'] = models
+
+    return res

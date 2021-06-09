4d6684e14bf4c0202bcd14d02d9c8c62c89fda0e
Tuğrul Hasan Karabulut <tugrulkarabulut64@gmail.com>
2021-06-09 12:29:37 +0300

    try except

--- cosine_similarity_calculator.py
+++ cosine_similarity_calculator.py
@@ -14,21 +14,23 @@ class CosineSimilarityCalculator(MRJob):
 
 
     def mapper(self, _, line):
-        orig_doc_feats = [float(feat) for feat in self.options.doc_features.split(',')]
-        orig_doc_norm = math.sqrt(sum([f ** 2 for f in orig_doc_feats]))
+        try:
+            orig_doc_feats = [float(feat) for feat in self.options.doc_features.split(',')]
+            orig_doc_norm = math.sqrt(sum([f ** 2 for f in orig_doc_feats]))
 
-        line_values = list(csv.reader(StringIO(line), skipinitialspace=True))[0]
-        id = line_values[0]
-        doc_feats = [float(feat) for feat in line_values[1:]]
-        doc_norm = math.sqrt(sum([f ** 2 for f in doc_feats]))
-        for i in range(len(doc_feats)):
-            norm_multiple = orig_doc_norm * doc_norm
+            line_values = list(csv.reader(StringIO(line), skipinitialspace=True))[0]
+            id = line_values[0]
+            doc_feats = [float(feat) for feat in line_values[1:]]
+            doc_norm = math.sqrt(sum([f ** 2 for f in doc_feats]))
+            for i in range(len(doc_feats)):
+                norm_multiple = orig_doc_norm * doc_norm
 
-            if norm_multiple == 0:
-                norm_multiple = 1
+                if norm_multiple == 0:
+                    norm_multiple = 1
 
-            yield id, orig_doc_feats[i] * doc_feats[i] / (orig_doc_norm * doc_norm)
-
+                yield id, orig_doc_feats[i] * doc_feats[i] / (orig_doc_norm * doc_norm)
+        except Exception as e:
+            print(e)
         
     def reducer(self, key, values):
         yield key, math.cos(sum(values))
